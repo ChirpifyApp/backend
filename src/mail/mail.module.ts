17dcf028c9f4af9fb7da0b67831593cb5c7faf64
Jakub Bordáš <methalicity@gmail.com>
2023-06-17 14:46:20 +0200

    Revamped register confirmation email
    Closes #1

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
 	providers: [MailService],
 	exports: [MailService],
-	imports: [PrismaModule],
+	imports: [
+		PrismaModule,
+		MailerModule.forRoot({
+			transport: {
+				host: process.env.SMTP_HOST,
+				port: parseInt(process.env.SMTP_PORT),
+				secure: true,
+				auth: {
+					user: process.env.SMTP_USER,
+					pass: process.env.SMTP_PASS,
+				},
+			},
+			defaults: {
+				from: 'Chirpify <noreply@chirpify.xyz>',
+			},
+			template: {
+				dir: __dirname + '/templates',
+				adapter: new HandlebarsAdapter(),
+				options: {
+					strict: true,
+				},
+			},
+		}),
+	],
 })
 export class MailModule {}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,31 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterCode } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-
-const nodemailer = require('nodemailer');
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class MailService {
-	private transporter;
-
-	constructor(private prisma: PrismaService) {
-		this.transporter = nodemailer.createTransport({
-			host: process.env.SMTP_HOST,
-			port: process.env.SMTP_PORT,
-			secure: true,
-			auth: {
-				user: process.env.SMTP_USER,
-				pass: process.env.SMTP_PASS,
-			},
-		});
-	}
+	constructor(private prisma: PrismaService, private mailerService: MailerService) {}
 
 	async sendRegistrationCode(code: RegisterCode) {
-		return await this.transporter.sendMail({
-			from: 'Chirpify <noreply@chirpify.xyz>',
+		return await this.mailerService.sendMail({
 			to: await this.prisma.user.findFirst({ where: { id: code.userId } }).then((user) => user.email),
 			subject: 'Chirpify registration',
-			text: `Your registration link is ${process.env.FRONTEND_URL}/verify/${code.id}?code=${code.code}\nor use code ${code.code} on the verification page`,
+			template: './confirmation',
+			context: {
+				code: code.code,
+				url: `${process.env.FRONTEND_URL}/verify/${code.id}?code=${code.code}`,
+			},
 		});
 	}
 }
